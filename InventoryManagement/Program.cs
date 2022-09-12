c426c0fd6103bc73c729dc4ba567d33a4c1594ea
pkr187 <pankajroman187@gmail.com>
2022-09-12 23:15:28 +0530

    [pankaj] Add:Display-of-Inventory-Details

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagement;
+const string INVENTORY_DATA_FILE_PATH = @"D:\RFP\PP\ObjectOrientedPrograms\ObjectOrientedPrograms\InventoryManagement\Inventory.json";
+while (true)
+{
+    Console.WriteLine("\n Select Program\n 1.Inventory Management");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Inventory inventory = new Inventory();
+            inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+            break;
+    }
+}
